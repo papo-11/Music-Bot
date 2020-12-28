1b13b657347353bf25c59aeb5791ff4dd4f267a8
papo-11 <71197775+papo-11@users.noreply.github.com>
2020-12-28 14:40:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-exports.Token = ""; //Bot Token - Important
-exports.Owner = ""; //Bot Owner ID - Important
-exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
-exports.Color = ""; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
-exports.Support = ``; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
-exports.Donate = `https://www.paypal.com/paypalme/legendemoji`; //Please Don't Change It, If You Want To Support Me <3
+exports.Token = "NzkyNDg2MTIwNzU0OTA1MTM5.X-eaNg.4bucrzCE-yXKFskHqkgzljB7I0M"; //Bot Token - Important
+exports.Owner = "731952429238714378"; //Bot Owner ID - Important
+exports.Default_Prefix = "b/"; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = "BLUE"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Support = `https://discord.gg/TzTf2R4Nsw`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
